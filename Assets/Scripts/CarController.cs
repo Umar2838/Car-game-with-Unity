40846841c1d5d32ff988906077d17e112c378f36
Umar2838 <umaraqeel1481@gmail.com>
2024-12-01 20:37:12 +0500

    Car is able to brake with keyboard

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -11,9 +11,13 @@ public class CarController : MonoBehaviour
     public Transform frontLeftWheelTransform;
     public Transform backRightWheelTransform;
     public Transform backLeftWheelTransform;
+
+    public Transform carCentreOfMassTransform;
+    public Rigidbody rigidBody;
     
     public float motorForce = 100f;
     public float steeringAngle = 30f;
+    public float brakeForce = 1000f;
 
     float veticalInput;
     float horizontalInput;
@@ -22,7 +26,7 @@ public class CarController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidBody.centerOfMass = carCentreOfMassTransform.localPosition;
     }
 
     // Update is called once per frame
@@ -32,12 +36,28 @@ public class CarController : MonoBehaviour
         UpdateWheels();
         GetInput();
         Steering();
+        ApplyBrakes();
     }
     void GetInput(){
         // GetAxis method is used to take input from keyboard vertical for up , down , W , S and Horizontal for right , left , A , D
         veticalInput =  Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
 
+    } 
+    void ApplyBrakes(){
+        if (Input.GetKey(KeyCode.Space)){
+        frontRightWheelCollider.brakeTorque = brakeForce;
+        frontLeftWheelCollider.brakeTorque = brakeForce;
+        backRightWheelCollider.brakeTorque = brakeForce;
+        backLeftWheelCollider.brakeTorque = brakeForce;
+        }
+        else{
+                 frontRightWheelCollider.brakeTorque = 0f ;
+        frontLeftWheelCollider.brakeTorque = 0f;
+        backRightWheelCollider.brakeTorque = 0f;
+        backLeftWheelCollider.brakeTorque = 0f;
+        }
+        
     }
     void MotorForce() {
         // Move the collider by motorTorque
